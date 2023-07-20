9a1b9c81032cbdf2e96c125de9bd42471d30078a
samarjeet04 <99862615+samarjeet04@users.noreply.github.com>
2023-07-20 17:04:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -139,3 +139,77 @@ for i, entity in enumerate(entities, 1):
     progress_bar.progress(progress)  # Update the progress bar
 
 
+
+import requests
+import streamlit as st
+
+# Function to make the API call to IICS
+def api_call_iics(username, password):
+    # Define the API endpoint and job details
+    session_url = ""
+    api_url = ""
+    # Define the request payload
+    payload1 = {
+        "username": username,
+        "password": password
+    }
+    response = requests.post(session_url, json=payload1, headers={"Authorization": "Access Token"})
+    if response.status_code == 200:
+        response_data = response.json()
+        IDS_SESSION_ID = response_data.get("sessionId")
+        return IDS_SESSION_ID
+    else:
+        return None
+
+# Streamlit app
+st.title('PDM Execution Engine')
+
+# Page 1: Login
+if 'page' not in st.session_state:
+    st.session_state['page'] = 'login'
+
+if st.session_state['page'] == 'login':
+    st.header('Login')
+    with st.form('login_form'):
+        username = st.text_input('Enter the User Name')
+        password = st.text_input('Enter a password', type='password')
+        submitted = st.form_submit_button('Login')
+
+    if submitted:
+        if username and password:
+            IDS_SESSION_ID = api_call_iics(username, password)
+            if IDS_SESSION_ID:
+                st.session_state['logged_in'] = True
+                st.session_state['IDS_SESSION_ID'] = IDS_SESSION_ID
+                st.session_state['page'] = 'api_call'
+            else:
+                st.error('Login failed. Please check your credentials.')
+        else:
+            st.warning('Please enter all the required details.')
+
+# Page 2: API Call
+if st.session_state['page'] == 'api_call':
+    if 'logged_in' in st.session_state:
+        if st.session_state['logged_in']:
+            st.header('API Call')
+            st.write('Welcome to the second page.')
+            # Make the second API call using IDS_SESSION_ID
+            IDS_SESSION_ID = st.session_state['IDS_SESSION_ID']
+            payload2 = {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "IDS-SESSION-ID": IDS_SESSION_ID
+            }
+            response2 = requests.request("POST", api_url, headers=payload2)
+
+            if response2.status_code == 200:
+                st.success('API call successful.')
+                st.write('Response2:', response2.json())
+            else:
+                st.error('API call failed.')
+
+# Error message if not logged in
+if 'logged_in' not in st.session_state:
+    st.error('Please log in to continue.')
+
+
