fab147a820371c39a3c497d51d408cdb14812ad5
samarjeet04 <99862615+samarjeet04@users.noreply.github.com>
2023-07-20 11:42:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import base64
+import json
 import PyPDF2
+import streamlit as st
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
 from langchain.text_splitter import CharacterTextSplitter 
-import streamlit as st
 from constants import openai_key
 from langchain.chains import LLMChain
 
@@ -32,9 +33,6 @@ def add_logo_from_local(image_file):
 
     st.empty().markdown(logo, unsafe_allow_html=True)
 
-add_logo_from_local(os.path.join(os.path.dirname(__file__), "images\logo.png"))
-  
-
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
@@ -49,14 +47,15 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
+
+add_logo_from_local(os.path.join(os.path.dirname(__file__), "images\logo.png"))
 add_bg_from_local('images\BG_IMAGE3.jpg')
 
-st.title('Entity Extractor')
+st.title('Entity Identifier')
 
 with st.form('my_form'):
-  uploaded_pdf = st.file_uploader(label='Upload PDF:', type='pdf')
-  submitted = st.form_submit_button('Submit')
-  
+    uploaded_pdf = st.file_uploader(label='Upload PDF:', type='pdf')
+    submitted = st.form_submit_button('Submit')
 
 template = '''You are an asssistant whose goal is to extract the following entities:
          Given text:
@@ -68,15 +67,28 @@ template = '''You are an asssistant whose goal is to extract the following entit
         2. Monetary Entity
         3. Location Entity
         4. Date Entity
+
+        Format the output as JSON with the following information:
+        Entity Name,
+        Entity Type,
+        Start Index of Entity,
+        End Index of Entity
+
+        strictly use the following keys in json
+        Entity_Name,
+        Entity_Type,
+        Start_Index,
+        End_Index
+
         '''
 
 entity_prompt = PromptTemplate(
-        input_variables=['passage'],
-        template=template
-        )
+    input_variables=['passage'],
+    template=template
+)
+
 llm = OpenAI(temperature=0)
-chain = LLMChain(llm=llm, prompt=entity_prompt,
-                        verbose=True, output_key='entity')
+chain = LLMChain(llm=llm, prompt=entity_prompt, verbose=True, output_key='entity')
 
   
 def read_pdf(file): 
@@ -99,19 +111,29 @@ def split_text(text):
 
 if submitted and uploaded_pdf: 
     pdf_text = read_pdf(uploaded_pdf)
-    # Split the text into chunks
+    # Split the text into chunks    
     text_chunks = split_text(pdf_text)
 
-# Extract entities from each text chunk
+    # Extract entities from each text chunk
     entities = []
     for chunk in text_chunks:
-        result = chain.run(passage =chunk)
+        result = chain.run(passage=chunk)
         entities.append(result)
 
-# Display entities
+    json_data = json.dumps(entities, indent=4)
+
+    st.download_button(
+        label="Download JSON File",
+        data=json_data.encode("utf-8"),
+        file_name="extracted_entities.json",
+        mime='application/json'
+    )
+
+    # Display entities in an expander
     st.subheader('Extracted Entities')
-    for entity in entities:
-        st.text(f'Entity: {entity}, Type: {chain.output_key}')
+    for i, entity in enumerate(entities, 1):
+        with st.expander(f'Entity {i}', style='background-color: white'):
+            st.json(entity)
 
 
 
